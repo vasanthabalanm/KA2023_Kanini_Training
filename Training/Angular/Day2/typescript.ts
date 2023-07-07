04cf3b34948b888bcd499e900cdea7cdfc9d44bb
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-07-07 15:22:12 +0530

    day2 angular

---
+++ Training/Angular/Day2/typescript.ts
@@ -0,0 +1,13 @@
+// let message: string = "Hello, world!";
+// console.log(message);
+
+
+// var str ="welcome to typescript";
+// function fun(){
+//     console.log(str);
+// }
+// fun();
+
+function getTime(): number {
+    return new Date().getTime();
+  };
