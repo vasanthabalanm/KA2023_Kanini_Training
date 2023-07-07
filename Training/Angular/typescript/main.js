a3e2d524e497f5654462e42053871132182723ae
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-07-07 17:37:09 +0530

    typescript concept

---
+++ Training/Angular/typescript/main.js
@@ -0,0 +1,14 @@
+//number type
+var age = 30;
+console.log(age);
+var pi = 3.14;
+console.log(pi);
+//string type
+var nme = "rama";
+console.log(nme);
+//boolean type
+var ckh = true;
+console.log(ckh);
+//array type
+var numbrs = [1, 2, 3, 4, 5];
+console.log(numbrs);
