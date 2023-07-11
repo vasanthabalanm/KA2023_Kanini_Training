ba9741203f61820fd2be3465268de94e2647173e
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-07-11 12:46:51 +0530

    N_unit Setup/teardown and order annotations updated

--- Training/Testing/Unit_Testing/Tests/Calaculation_Tests.cs
+++ Training/Testing/Unit_Testing/Tests/Calaculation_Tests.cs
@@ -11,10 +11,23 @@ namespace Tests
     [TestFixture]
     public class Calaculation_Tests
     {
+
+        //for setup annotations put in top
+        private Calculation? calculation;
+        [SetUp] 
+        public void SetUp() 
+        {
+            calculation = new Calculation(100, 100);
+            Console.WriteLine("Setup");
+        }
+
+
         [Test]
         [Order(2)]
         public void Add_Tests()
         {
+            Console.WriteLine("Add order check");
+
             Calculation calculation = new Calculation(100,100);
             int res = calculation.Add();
             Assert.AreEqual(200,res);
@@ -24,6 +37,8 @@ namespace Tests
         [Order(3)]
         public void Sub_tests()
         {
+            Console.WriteLine("sub order check");
+
             Calculation calculation = new(40, 30);
             int res = calculation.Sub();
             Assert.AreEqual(10,res);
@@ -33,9 +48,19 @@ namespace Tests
         [Order(1)]
         public void Mul_tests()
         {
+            Console.WriteLine("mult order check");
+
             Calculation calculation = new(40, 30);
             int res = calculation.Mul();
             Assert.AreEqual(1200, res);
+        }
+
+        //for teardown annotations in down of the all tests are completed
+        [TearDown]
+        public void Closetest()
+        {
+            Console.WriteLine("teardown");
+            calculation = null;
         }
     }
 }
